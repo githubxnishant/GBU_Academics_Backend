45914bca67512afba44cd9120ec307e04bda31cc
Nishant Chauhan <mailxnishant@gmail.com>
2025-02-09 17:51:33 +0530

    update env

--- data/db.js
+++ data/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(process.env.DB_URI, {
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI, {
             dbName: 'gbu_academics',
         });
         console.log(`MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
