583cace52e305637c6f2c070f4c416cf8d496fb3
Nishant Chauhan <mailxnishant@gmail.com>
2025-02-09 15:41:01 +0530

    make connection

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ import { connectDB } from "./data/db.js";
 connectDB();
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port: ${process.env.PORT} in ${process.env.NODE_ENV} Mode.`);
+    console.log(`Server is running on port: ${process.env.PORT}`);
 })
